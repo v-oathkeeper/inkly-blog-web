c93e74e92a03e64557b33fe81899c270aca5244b
v-oathkeeper <vishnuvichu8150@gmail.com>
2025-07-15 23:40:18 +0530

    Initial commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+  return (
+    <div className="mx-8 sm:mx-16 xl:mx-24 relative">
+      <div className="text-center mt-20 mb-8">
+        <div
+          className="inline-flex items-center justify-center gap-4 px-6 py-1.5
+        mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm
+        text-primary"
+        >
+          <p>New: AI feature integrated</p>
+          <img src={assets.star_icon} alt="star" className="w-2.5" />
+        </div>
+
+        <h1
+          className="text-3xl sm:text-6xl font-semibold sm:leading-16
+        text-gray-700"
+        >
+          Your story deserves a home. Start writing.
+        </h1>
+
+        <p
+          className="my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs
+        text-gray-500"
+        >
+          This is your personal writing space — built for clarity, creativity,
+          and control. Whether you're sharing ideas, journaling thoughts, or
+          publishing stories, it’s all yours. No algorithms. No distractions.
+          Just your voice, unfiltered.
+        </p>
+      </div>
+      <img
+        src={assets.gradientBackground}
+        alt="background"
+        className="absolute -top-50 -z-1
+      opacity-50"
+      />
+    </div>
+  );
+}
+
+export default Header
